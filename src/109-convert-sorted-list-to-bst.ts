79f029870ec97c8df06d54b31fe7dbb5468a4750
Bárbara Este <barbaraeste01@gmail.com>
2023-03-11 19:18:08 -0300

    add problem 109 solution using AVL Tree implementation

---
+++ src/109-convert-sorted-list-to-bst.ts
@@ -0,0 +1,121 @@
+/**
+ * @see https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/description/
+ *
+ * Given the head of a singly linked list where elements are sorted in ascending order,
+ * convert it to a height-balanced binary search tree.
+ *
+ * Constraints:
+ *  - The number of nodes in `head` is in the range [0, 2 * 10^4]
+ *  - -10^5 <= Node.val <= 10^5
+ *
+ * Complexity Analysis
+ *  - Space complexity: is O(n) because we create a binary tree of N nodes
+ *  - Time complexity: is O(log n) due to AVL Tree data structure calculations, it is based
+ *  on the tree's height.
+ */
+
+import { LinkedListNode as ListNode } from "../schema/linked-list";
+import { BinaryTreeNode as TreeNode } from "../schema/binary-tree";
+
+function sortedListToBST(head: ListNode | null): TreeNode | null {
+  // The problem states that it wants a "heigh-balanced binary search tree",
+  // meaning that we need to implement the insertion algorithm in a way
+  // that the tree becomes balanced after the insertion of a node, i. e. we
+  // need to implement an AVL tree.
+
+  /**
+   * The nomenclature here was used to mantain an standard, over
+   * inumerous examples used `z`, `y` and `T` as the default terminology
+   * so I thought of keeping the same names.
+   * */
+  function leftRotate(z: TreeNode): TreeNode {
+    const y = z.right as TreeNode;
+    const T2 = y.left;
+    y.left = z;
+    z.right = T2;
+    return y;
+  }
+
+  function rightRotate(z: TreeNode): TreeNode {
+    const y = z.left as TreeNode;
+    const T3 = y.right;
+    y.right = z;
+    z.left = T3;
+    return y;
+  }
+
+  function getHeight(treeNode: TreeNode | null): number {
+    if (!treeNode) {
+      return -1;
+    }
+
+    return 1 + Math.max(getHeight(treeNode.left), getHeight(treeNode.right));
+  }
+
+  function getBalanceFactor(treeNode: TreeNode | null): number {
+    return treeNode ? getHeight(treeNode.left) - getHeight(treeNode.right) : 0;
+  }
+
+  function addNodeToTree(root: TreeNode | null, node: TreeNode): TreeNode {
+    if (!root) {
+      return node;
+    }
+
+    if (node.val < root.val) {
+      root.left = addNodeToTree(root.left, node);
+    } else {
+      root.right = addNodeToTree(root.right, node);
+    }
+
+    root = balanceTree(root);
+    return root;
+  }
+
+  function balanceTree(root: TreeNode): TreeNode {
+    const balanceFactor = getBalanceFactor(root);
+    const leftSubtreeBalanceFactor = getBalanceFactor(root.left);
+    const rightSubtreeBalanceFactor = getBalanceFactor(root.right);
+
+    // The tree is balanced.
+    if (Math.abs(balanceFactor) <= 1) {
+      return root;
+    }
+
+    // The tree is unbalaced ("right-heavy") and
+    // the unbalanced node is a "left" child of the parent Node (case "Left Left")
+    if (balanceFactor > 1 && leftSubtreeBalanceFactor > 0) {
+      return rightRotate(root);
+    }
+
+    // The tree is unbalanced ("right-heavy") and
+    // the unbalanced node is a "right" child of the parent Node (case "Left Right")
+    if (balanceFactor > 1 && leftSubtreeBalanceFactor < 0) {
+      root.left = leftRotate(root.left as TreeNode);
+      return rightRotate(root);
+    }
+
+    // The tree is unbalanced ("left-heavy") and
+    // the unbalanced node is a "right" child of the parent Node (case "Right Left")
+    if (balanceFactor < -1 && rightSubtreeBalanceFactor > 0) {
+      root.right = rightRotate(root.right as TreeNode);
+      return leftRotate(root);
+    }
+
+    // The tree is unbalanced ("left-heavy") and
+    // the unbalanced node is a "left" child of the parent Node (case "Left Left")
+    if (balanceFactor < -1 && rightSubtreeBalanceFactor < 0) {
+      return leftRotate(root);
+    }
+
+    return root;
+  }
+
+  let root: TreeNode | null = null;
+
+  while (head) {
+    root = addNodeToTree(root, new TreeNode(head.val));
+    head = head.next;
+  }
+
+  return root;
+}
