495fec3fbed4126fbe4307adf27d665cbf6da45a
Bárbara Este <barbaraeste01@gmail.com>
2024-09-20 22:55:30 -0300

    add 215 Kth largest element in array solution

---
+++ src/215-kth-largest-element-in-array.ts
@@ -0,0 +1,12 @@
+import { MaxHeap } from "../schema/max-heap";
+
+export function findKthLargest(nums: number[], k: number): number {
+  const maxHeap = MaxHeap.build(nums);
+  let kthLargest = -1;
+
+  for (let i = 0; i < k; i++) {
+    kthLargest = maxHeap.extractMaximum();
+  }
+
+  return kthLargest;
+}
