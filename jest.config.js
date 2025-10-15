9c6755380e6a2d96773f2d10f86163d76e4f757d
Bárbara Este <barbaraeste01@gmail.com>
2025-10-15 16:28:29 -0300

    chore: update Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  testMatch: ["**/src/**/*.spec.ts"],
   collectCoverage: true,
   collectCoverageFrom: ["*.ts"],
   coverageDirectory: "../coverage",
