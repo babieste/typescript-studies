e9529056ac053d04e37b9957152510ccf241e8f1
Bárbara Este <barbaraeste01@gmail.com>
2025-11-11 20:43:51 -0300

    Add complexity analysis to problem 122: Best Time to Buy and Sell Stock II

--- src/122-best-time-to-buy-and-sell-stock-ii.ts
+++ src/122-best-time-to-buy-and-sell-stock-ii.ts
@@ -12,7 +12,17 @@
  * - `1 <= prices.length <= 3 * 10^4`
  * `0 <= prices[i] <= 10^4`
  *
+ * #### Reasoning
+ *
+ * The key insight that simplifies the problem is that because you can make unlimited transactions,
+ * you don't need to worry about finding the absolute lowest price to buy and the absolute highest
+ * price to sell over the entire period. Instead, you should simply capture every single price
+ * increase you encounter.
+ *
  * #### Complexity Analysis
+ * - Time complexity: we iterate the prices array once, and do constant time operations at each
+ * index - `O(N)`;
+ * - Space complexity: an additional array `profit` of length N was created - `O(N)`.
  */
 export function maxProfit(prices: number[]): number {
     const n = prices.length;
@@ -24,4 +34,21 @@ export function maxProfit(prices: number[]): number {
     }
 
     return profit[n - 1];
+}
+
+/**
+ * Since to populate the `profit` array on a specific day depends only
+ * on the profit of the previous day, the array can be transformed into
+ * a single variable `maxProfit`, which transforms the space complexity
+ * to `O(1)`.
+ */
+export function maxProfit_enhanced(prices: number[]): number {
+    const n = prices.length;
+    let maxProfit = 0;
+
+    for (let day = 1; day < n; day++) {
+        maxProfit = maxProfit + Math.max(prices[day] - prices[day - 1], 0);
+    }
+
+    return maxProfit;
 }
